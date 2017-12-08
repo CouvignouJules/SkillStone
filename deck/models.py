e5181e656e5761da822f70751473636fe149865a
Sonlight <louis.beaumont@gmail.com>
2017-12-08 11:54:16 +0100

    Add models

--- deck/models.py
+++ deck/models.py
@@ -1,12 +1,50 @@
 from django.db import models
 
 
-class Deck(models.Model):
+
+
+
+class CardType(models.Model)
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=30)
+
+
+class Effect(models.model)
+	id models.AutoField(primary_key=True)
+	name = models.CharField(max_length=30)
 
 
 class Card(models.Model):
-	
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=30)
+	description = models.CharField(max_length=100)
+	cost = models.IntegerField()
+	attack = models.IntegerField()
+	health = models.IntegerField()
+	cardType = models.ForeignKey(CardType, on_delete=models.CASCADE)
+	effect = models.ManyToMany(Effect)
 
+
+class Player(models.Model):
+	pseudo = models.CharField(max_length=30)
+	life = models.IntegerField(default=30)
+	hand = models.ManyToManyField(Card)
+	deck = models.ManyToManyField(Card)
+
+	def setLife(self, life):
+		self.life = life
+
+	def setPseudo(self, pseudo):
+		if pseudo.length() > 0
+			self.pseudo = pseudo
+
+	def addCardToHand(self, card):
+		if hand.count() < 11
+			self.hand.add(card)
+
+	def addCardToDeck(self, card):
+		if deck.count() < 31
+			self.deck.add(card)
 
 
 # Create your models here.
--- deck/views.py
+++ deck/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Card
+from .models import Player
 # Create your views here.
+
+
+
+
+
+def deck(request):
+	Cards = Card.objects.all()
+	#TODO : get player from session and put his hand in a var
+	#resultSet2 = Player.objects.first().hand
+	return render(request,'SkillStone/deck.html',locals())
