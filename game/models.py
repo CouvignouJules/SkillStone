a1f9225456b06259aed59cdca82b20ae5ce917a8
Thomas Alunni-Gradini <thomas.alunni-gradini@ynov.com>
2017-12-15 10:38:21 +0100

    add

--- game/models.py
+++ game/models.py
@@ -3,6 +3,27 @@ from django.db import models
 # Create your models here.
 
 
+class CardType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+
+class Effect(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+
+class Card(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    cost = models.IntegerField()
+    attack = models.IntegerField()
+    health = models.IntegerField()
+    cardType = models.ForeignKey(CardType, on_delete=models.CASCADE)
+    effect = models.ManyToManyField(Effect)
+
+
 class Player(models.Model):
     pseudo = models.CharField(max_length=30)
     life = models.IntegerField(default=30)
