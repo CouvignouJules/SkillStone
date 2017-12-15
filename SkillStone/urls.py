fe1fd9a68604d2e992ff357e2a54fef717342ca6
Thomas Alunni-Gradini <thomas.alunni-gradini@ynov.com>
2017-12-15 16:40:42 +0100

    init game app

--- SkillStone/urls.py
+++ SkillStone/urls.py
@@ -18,16 +18,20 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from HMAuth import views as hmauth_views
 from deck import views as deck_views
+from game import views as game_views
 from SkillStone import views as skill_views
 
 
 urlpatterns = [
     url(r'^$', hmauth_views.home, name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^login/$', auth_views.login,
+        {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout,
+        {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', hmauth_views.signup, name='signup'),
     url(r'^accounts/profile/$', skill_views.profile, name='profile'),
     url(r'^deck/$', deck_views.deck, name='deck'),
+    url(r'^game/$', game_views.game, name='game'),
     url(r'^admin/', admin.site.urls),
 
 ]
--- deck/views.py
+++ deck/views.py
@@ -6,7 +6,7 @@ from .models import Player
 
 
 def deck(request):
-	Cards = Card.objects.all()
-	#TODO : get player from session and put his hand in a var
-	#resultSet2 = Player.objects.first().hand
-	return render(request,'deck.html',locals())
+    Cards = Card.objects.all()
+    # TODO : get player from session and put his hand in a var
+    #resultSet2 = Player.objects.first().hand
+    return render(request, 'deck.html', locals())
