5e7fedc15c32dc6a69f1ff3d6aa2676c47f464da
Thomas Alunni-Gradini <thomas.alunni-gradini@ynov.com>
2018-01-11 09:15:35 +0100

    admin backoffice

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CardType, CardTypeAdmin, Effect, EffectAdmin, Card, CardAdmin, Player, PlayerAdmin
 
 # Register your models here.
+
+admin.site.register(CardType, CardTypeAdmin)
+admin.site.register(Effect, EffectAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(Player, PlayerAdmin)
