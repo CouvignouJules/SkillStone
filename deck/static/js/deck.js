10c18ec63e8231413bc3dd9f3df1b517f2e35a73
Jules COUVIGNOU <julesCouv@ynov.com>
2018-02-25 19:51:37 +0100

    grud deck operationnel

--- deck/static/js/deck.js
+++ deck/static/js/deck.js
@@ -16,47 +16,54 @@ $( document ).ready(function() {
         var nom = $('#nomdeck').val();
         var jsondeck = JSON.stringify(deck);
         var data = "{\"name\":\""+nom+"\",\"cards\":"+jsondeck+"}";
-
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:8000/mydeck/deck",
-            dataType: "json",
-            headers: {
-                "Authorization": 'Token '+token,
-                "X-CSRFToken": csrftoken,
-                "Content-Type":'application/json'
-            },
-            data : data,
-            success: function (msg) {
-                console.log('Success!',msg)
-                window.location.reload()
-            },
-            error : function(err) {
-                console.log('Error!', err)
-            }
-        });
+        sendDeck(data)
     });
 
     $("#select-deck").on('change', function() {
-      if($("#select-deck").val() == "") {
-        $("#modifDeck").hide();
-        $("#suprDeck").hide();
-        $("#senddeck").show();
-        $('#nomdeck').val("Nouveau Deck");
-        $('#newdeck').empty()
-      } else {
-        $("#modifDeck").show();
-        $("#suprDeck").show();
-        $("#senddeck").hide();
-        getDeck($("#select-deck").val(),function (data) {
+        if($("#select-deck").val() == "") {
+            deck = [];
+            $("#modifDeck").hide();
+            $("#suprDeck").hide();
+            $("#senddeck").show();
+            $('#nomdeck').val("Nouveau Deck");
             $('#newdeck').empty()
-            $('#nomdeck').val($("#select-deck option:selected").text());
-            data.forEach(function(element) {
-                console.log(element.name);
-                $('#newdeck').append("<div class='deckCard' id="+element.id+"><img title='"+element.name+"' src='"+element.img+"' style='width: 200px; height: 290px;' /></div>")
+        } else {
+            deck = [];
+            $("#modifDeck").show();
+            $("#suprDeck").show();
+            $("#senddeck").hide();
+            getDeck($("#select-deck").val(),function (data) {
+                $('#newdeck').empty()
+                $('#nomdeck').val($("#select-deck option:selected").text());
+                var contCart = 0
+                data.forEach(function(element) {
+                    console.log(element.name);
+                    deck.push(parseInt(element.id));
+                    $('#newdeck').append("<div class='deckCard' id='card-"+contCart+"'><img title='"+element.name+"' src='"+element.img+"' style='width: 200px; height: 290px;' /></div>")
+                    contCart += 1;
+                });
             });
-        });
-      }
+        }
+    });
+
+    $(document).on('click', '.deckCard', function () {
+        var id = this.id;
+        id = id.split('-')[1];
+        delete deck[id];
+        $('#card-' + id).remove();
+    });
+
+    $("#suprDeck").on('click', function () {
+        suprDeck($('#select-deck').val());
+    });
+    
+    $("#modifDeck").on('click', function () {
+        var nom = $('#nomdeck').val();
+        deck = cleanArray(deck);
+        var jsondeck = JSON.stringify(deck);
+        var data = "{\"name\":\""+nom+"\",\"cards\":"+jsondeck+"}";
+        sendDeck(data);
+        suprDeck($('#select-deck').val());
     });
 });
 
@@ -94,4 +101,55 @@ function getDeck(id,callback) {
             console.log('Error!', err)
         },
     });
+}
+
+function  suprDeck(id) {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8000/mydeck/deck/"+id,
+        dataType: "json",
+        headers: {
+            "Authorization": 'Token '+token,
+            "X-CSRFToken": csrftoken,
+            "Content-Type":'application/json'
+        },
+        success: function (msg) {
+            console.log('Success! deck creer',msg)
+            window.location.reload()
+        },
+        error : function(err) {
+            console.log('Error!', err)
+        }
+    });
+}
+
+function sendDeck(data) {
+    $.ajax({
+            type: "POST",
+            url: "http://localhost:8000/mydeck/deck",
+            dataType: "json",
+            headers: {
+                "Authorization": 'Token '+token,
+                "X-CSRFToken": csrftoken,
+                "Content-Type":'application/json'
+            },
+            data : data,
+            success: function (msg) {
+                console.log('Success! deck suprimer',msg)
+                window.location.reload()
+            },
+            error : function(err) {
+                console.log('Error!', err)
+            }
+        });
+}
+
+function cleanArray(actual) {
+  var newArray = new Array();
+  for (var i = 0; i < actual.length; i++) {
+    if (actual[i]) {
+      newArray.push(actual[i]);
+    }
+  }
+  return newArray;
 }
